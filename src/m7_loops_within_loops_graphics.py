4bd7dbaafd2b84a1da190e0f52ca1e4024b6b7cc
jacobsgm <jacobsgm@rose-hulman.edu>
2018-01-31 20:13:19 -0500

    these are tricky

--- src/m7_loops_within_loops_graphics.py
+++ src/m7_loops_within_loops_graphics.py
@@ -212,10 +212,12 @@ def many_hourglasses(window, square, m, colors):
     # ------------------------------------------------------------------
 
 
+
     hourglass(window, 1, square.center,square.length_of_each_side/2,'red')
     newrect = rg.Rectangle(rg.Point(square.center.x-square.length_of_each_side/2,
-                           square.center.y - square.length_of_each_side / 2)),rg.Point(square.center.x+square.length_of_each_side/2,
+                           square.center.y - square.length_of_each_side/2)), rg.Point(square.center.x+square.length_of_each_side/2,
                            square.center.y + square.length_of_each_side / 2)
+    newrect.attach_to(window)
 
 
 
